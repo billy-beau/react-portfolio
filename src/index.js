d819be7736eeadb7b6e27fb702d479b2b0cd6a8f
Billy-Beau <ksvineethkumar98@gmail.com>
2024-06-07 23:26:18 +0530

    Added vercel speed insights

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from './app/App';
 import './index.css';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
