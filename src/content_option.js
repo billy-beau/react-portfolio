23aec2abd24271b67df34a2acd7fbcb615570ac3
Billy-Beau <ksvineethkumar98@gmail.com>
2024-06-09 13:36:58 +0530

    My details are updated and need to update images

--- src/content_option.js
+++ src/content_option.js
@@ -1,38 +1,38 @@
 const logotext = "VINEETH";
 const meta = {
     title: "Vineeth Kumar K S",
-    description: "I’m Vineeth Kumar K S data scientist _ Full stack devloper,currently working in Berlin",
+    description: "I’m Vineeth Kumar K S Test Engineer currently working in Infosys, Chennai",
 };
 
 const introdata = {
     title: "I’m Vineeth Kumar K S",
     animated: {
-        first: "I'm a successful tester",
-        second: "Having interest on Network Engineer",
-        third: "I can develop websites",
+        first: "E2E Tester",
+        second: "But I'm Passionate on SOC Analyst",
+        third: "Finally I can design websites",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "I have been working as an E2E Test Engineer at Infosys for the past 2.5 years, ensuring the quality and reliability of software systems. With a strong interest in technology and cybersecurity, I aspire to become a SOC Analyst. I am passionate about continuous learning and look forward to contributing to cybersecurity in future roles.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
     title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    aboutme: "My name is Vineeth Kumar K S, and I hail from the vibrant city of Madurai, India. I am currently working as an E2E Test Engineer at Infosys, a position I have held for the past 2.5 years. My role has provided me with a wealth of experience in end-to-end testing, ensuring seamless integration and functionality of complex systems. Looking towards the future, I am keenly interested in transitioning into the field of Security Operations Center (SoC) Analysis. This career path excites me as it aligns with my passion for cybersecurity and the dynamic challenges it presents. In my personal time, I find joy in listening to music, which serves as a relaxing escape from the demands of work. I am also an avid traveler, always eager to explore new places and cultures, which has broadened my perspective and enriched my life experiences. As I continue to grow both professionally and personally, I am driven by a commitment to excellence and a desire to make meaningful contributions to the field of cybersecurity. I look forward to the new opportunities and challenges that lie ahead.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Test Engineer",
+        where: "Infosys, Chennai",
+        date: "January 2022 - Till Date",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "Operations Associate",
+        where: "Solartis, Madurai",
+        date: "December 2020 - October 2021",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "Microsoft's Advocate (Freelance)",
+        where: "Limitless",
+        date: "March 2020 - December 2020",
     },
 ];
 
@@ -142,7 +142,7 @@ const dataportfolio = [{
 const contactConfig = {
     YOUR_EMAIL: "ksvineethkumar98@gmail.com",
     YOUR_FONE: "+91 6380042809",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    description: "If you're interested in my profile or believe I am a suitable candidate for your current openings, please contact me via the provided email or phone. Alternatively, you may fill out the contact form. Thank you for your consideration. ",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "default_service",
@@ -151,10 +151,10 @@ const contactConfig = {
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    github: "https://github.com/billy-beau",
+    instagram: "https://www.instagram.com/vineethkumarks/?hl=en",
+    linkedin: "https://linkedin.com/in/vineethkumar-ks",
+    // twitter: "https://twitter.com",
 };
 export {
     meta,
